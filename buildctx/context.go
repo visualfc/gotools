56fadda53a863d81226000630ac5374312485974
visualfc <visualfc@gmail.com>
2018-04-09 16:09:19 +0800

    change context->buildctx

--- buildctx/context.go
+++ buildctx/context.go
@@ -1,4 +1,8 @@
-package context
+// Copyright 2011-2018 visualfc <visualfc@gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package buildctx
 
 import (
 	"go/build"
