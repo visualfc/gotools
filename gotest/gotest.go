b35de216c4dfe498303d976b0ad5bf1422ef2c9e
Christoph Pech <christoph.pech@gmail.com>
2019-02-15 14:44:28 +0100

    apply build tags (-tags) to default go build context

--- gotest/gotest.go
+++ gotest/gotest.go
@@ -25,9 +25,10 @@ var Command = &command.Command{
 var testFileName string
 var testFileArgs string
 
-//func init() {
-//	Command.Flag.StringVar(&testFileName, "f", "", "test go filename")
-//}
+func init() {
+	//Command.Flag.StringVar(&testFileName, "f", "", "test go filename")
+	ApplyBuildTags()
+}
 
 func runGotest(cmd *command.Command, args []string) error {
 	index := -1
@@ -96,4 +97,18 @@ func runGotest(cmd *command.Command, args []string) error {
 	command.Stderr = os.Stderr
 
 	return command.Run()
+}
+
+func ApplyBuildTags() {
+	nexttag := false
+	for _, arg := range os.Args[1:] {
+		if nexttag {
+			build.Default.BuildTags = strings.Split(arg, ",")
+			nexttag = false
+			continue
+		}
+		if arg == "-tags" {
+			nexttag = true
+		}
+	}
 }
